709ee49df95e4f4d359911429b10ee09bab6eeaf
kenshero <kenshero.k@gmail.com>
2018-04-21 21:38:47 +0700

    fixed part 1

--- js/states/Game.js
+++ js/states/Game.js
@@ -1,39 +1,27 @@
 var BabbyBird = BabbyBird || {};
 BabbyBird.GameState = {
 init: function() {
-    // จะเเก็บ barrier ไว้ในตัวแปรนี้
-    this.barrierPool = this.add.group();
-//ความเร็วที่ตัวเราจะตกลงพื้น
-    this.game.physics.arcade.gravity.y = 400;
+    // จะเก็บ barrier ไว้ในตัวแปรนี้
+    this.barrierPool = this.add.group()
 // เป็นตัวแปรไว้ตั้งค่า keyboard
     this.cursors = this.game.input.keyboard.createCursorKeys();
 //ความเร็วในการเคลื่อนที่
-    this.levelPlayerSpeed = 200;
+    this.levelPlayerSpeed = 150
 // ความเร็วตอนกระโดด
-    this.levelJumpSpeed = 200;
+    this.levelJumpSpeed = 150
   },
   create: function() {
     //moving background
-    this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'background');
-    this.background.tileScale.y = 4;
-    this.background.autoScroll(-this.levelPlayerSpeed/6, 0);
-    this.game.world.sendToBack(this.background);
+    this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'background')
+    this.background.tileScale.y = 4
+    this.background.autoScroll(-this.levelPlayerSpeed/6, 0)
+    this.game.world.sendToBack(this.background)
     //create the player
-    this.player = this.add.sprite(50, this.game.world.centerY, 'player');
-    this.player.anchor.setTo(0.5);
-    this.game.physics.arcade.enable(this.player);
+    this.player = this.add.sprite(50, this.game.world.centerY, 'player')
+    this.player.anchor.setTo(0.5)
+    this.game.physics.arcade.enable(this.player)
+    this.player.body.gravity.y = 400
   },
   update: function() {
-    if(this.player.alive) {
-      if(this.cursors.up.isDown || this.game.input.activePointer.isDown) {
-        this.playerJump();
-      }
-      if(this.player.top >= this.game.world.height || this.player.left <= 0 || this.player.top < -10) {
-        this.gameOver();
-      }
-    }
-  },
-  playerJump: function(){
-    this.player.body.velocity.y = -this.levelJumpSpeed;
   }
 };
