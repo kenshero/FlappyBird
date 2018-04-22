24122c600819c822e324fac1a5c88ac657c0022b
kenshero <kenshero.k@gmail.com>
2018-04-22 14:02:33 +0700

    fixed part 3

--- js/states/Game.js
+++ js/states/Game.js
@@ -21,18 +21,44 @@ init: function() {
     this.player.anchor.setTo(0.5)
     this.game.physics.arcade.enable(this.player)
     this.player.body.gravity.y = 400
+this.timer = this.game.time.events.loop(2500, this.createBarrier, this)
   },
   update: function() {
     if(this.player.alive) {
+      this.game.physics.arcade.overlap(this.player, this.barrierPool, this.gameOver, null, this)
       if(this.cursors.up.isDown || this.game.input.activePointer.isDown) {
-        this.playerJump();
+        this.playerJump()
       }
       if(this.player.top >= this.game.world.height || this.player.left <= 0 || this.player.top < -10) {
-        this.gameOver();
+        this.gameOver()
       }
-    }
+}
   },
   playerJump: function(){
     this.player.body.velocity.y = -this.levelJumpSpeed;
+  },
+  createBarrier: function() {
+    let yBarrier = 20
+    var hole = Math.floor(Math.random() * 5 + 1)
+    for(let i=0; i < 7; i++) {
+      if(i != hole) {
+        var barrier = this.add.sprite(300, yBarrier, 'barrier')
+        this.barrierPool.add(barrier)
+        this.game.physics.arcade.enable(barrier)
+        barrier.body.velocity.x = -150
+        barrier.checkWorldBounds = true
+        barrier.outOfBoundsKill = true
+      }
+      yBarrier += 100
+    }
+  },
+  gameOver: function() {
+    this.player.kill()
+    var style = {font: '30px Arial', fill: '#fff'}
+    this.add.text(this.game.width/2, this.game.height/2, 'GAME OVER', style).anchor.setTo(0.5)
+    this.game.input.onDown.addOnce(this.restart, this)
+  },
+  restart: function(){
+    this.game.state.start('Game')
   }
 };
